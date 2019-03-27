a765ea9f679371484670b36ee77757a038f59120
jjn42 <jjrr1234@aol.com>
2019-03-26 19:52:59 -0500

    ugh

--- myPortfolio/scripts/tictactoe.js
+++ myPortfolio/scripts/tictactoe.js
@@ -2,11 +2,11 @@
 // clear all x and o from the bord
 // decide who makes the first move x or o
 var moves=0;
-currentPlayer = 1;
+/*currentPlayer = 1;
 //function setUpBoard()
 function placeMark(elementId,marker){
  var box=document.getElementById(elementId);
- if currentPlayer ===1{
+ if currentPlayer === 1{
      if(box.innerHTML==="")
     box.innerHTML="x";
     moves=moves+1;}
@@ -40,4 +40,55 @@ clearBoard(){
     for(var i=1; i<10; i++)
     var box=document.getElementById(space1+i);
     box.innerHTML="";
+}*/
+
+var board;
+const xplayer = 'x';
+const aiplayer = 'o';
+const winCombos = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7 ,8],
+    [0, 3, 6],
+    [6, 4, 2],
+    [0, 4, 8],
+    [2, 5, 8],
+    [1, 4, 7]
+]
+
+const row = document.querySelectorAll('.row');
+startGame();
+
+function clearBoard() {
+ document.querySelector('end') .style.display = "none"
+ board = Array.from(Array(9).keys());
+ for (var i = 0; i < row.length; i++ ){
+     row[i].innerText = '';
+     row[i].styles.removeProperty(background-color);
+     row[i].addEventListener('click', turnClick, false);
+ }
+}
+
+function turnClick(square){
+   turn(square.target.id, xplayer)
+
+}
+function turn(squareId, player){
+    Board[squareId] = player;
+    document.getElementById(squareId).innerText = player;
+    let gameWon = checkWin(board, player)
+    if (gameWon) clearBoard(gameWon)
+}
+
+function checkWIn(board1, player) {
+    let plays = board.reduce((a, e, i) =
+     (e === player)) ? a.concat(i) : a, [])
+     let gameWon = null;
+     for (let [index, win] of winCombos.entries()){
+       id (win.every(elem = plays.indexOf(elem > -1)))  {
+        gameWOn = {index: index} player: player};   
+        break;
+       }
+     }
+      
 }
